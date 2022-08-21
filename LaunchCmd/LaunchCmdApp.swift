3625324b4700c5ba74c724cf67a725f6ceafab4a
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-21 12:12:54 -0400

    Initial Xcode template commit (macOS App). This specifically excludes: .//LaunchCmd.xcodeproj/xcuserdata .//LaunchCmd.xcodeproj/project.xcworkspace/xcuserdata

---
+++ LaunchCmd/LaunchCmdApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchCmdApp.swift
+//  LaunchCmd
+//
+//  Created by Roberto Machorro on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LaunchCmdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
