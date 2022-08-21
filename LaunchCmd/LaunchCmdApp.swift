e750cb9045ccaf95ba3160f647584889ca965456
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-21 12:32:31 -0400

    Fix whitespace formatting.

--- LaunchCmd/LaunchCmdApp.swift
+++ LaunchCmd/LaunchCmdApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct LaunchCmdApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
